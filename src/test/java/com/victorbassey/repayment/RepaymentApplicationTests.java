53f4c5d207dbd07819ab86bcb76150023bdd3402
Victor Bassey <viktorbasi545@gmail.com>
2020-04-07 08:40:11 +0100

    Initial project setup

---
+++ src/test/java/com/victorbassey/repayment/RepaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victorbassey.repayment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepaymentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
