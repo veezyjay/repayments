9ca2c46c165f9a0bd626f310e730ddb9a56d668a
Victor Bassey <viktorbasi545@gmail.com>
2020-04-07 09:13:37 +0100

    Add customer entity
    #1

---
+++ src/main/java/com/victorbassey/repayment/model/Customer.java
@@ -0,0 +1,36 @@
+package com.victorbassey.repayment.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "customers")
+public class Customer {
+    @Id
+    private Long customerId;
+
+    String customerName;
+
+    public Long getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Long customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "customerId=" + customerId +
+                ", customerName='" + customerName + '\'' +
+                '}';
+    }
+}
