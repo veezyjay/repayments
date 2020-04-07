9c31d3ea7f641faeb06a164d7edd491f4b0bb2b0
Victor Bassey <viktorbasi545@gmail.com>
2020-04-07 16:54:03 +0100

    Make return value an optional

--- src/main/java/com/victorbassey/repayment/repository/CustomerSummaryRepository.java
+++ src/main/java/com/victorbassey/repayment/repository/CustomerSummaryRepository.java
@@ -9,11 +9,12 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface CustomerSummaryRepository extends JpaRepository<CustomerSummary, Long> {
 
-    CustomerSummary findByCustomerIdAndSeasonId(Long customerId, Long seasonId);
+    Optional<CustomerSummary> findByCustomerIdAndSeasonId(Long customerId, Long seasonId);
 
     @Query("SELECT c FROM CustomerSummary c INNER JOIN Season s ON c.seasonId = s.seasonId " +
             "WHERE c.customerId = :customerId AND c.totalCredit > c.totalRepaid ORDER BY s.startDate")
@@ -21,5 +22,5 @@ public interface CustomerSummaryRepository extends JpaRepository<CustomerSummary
 
     @Query(value = "SELECT * FROM customer_summaries AS c INNER JOIN seasons AS s ON c.season_id = s.season_id " +
             "WHERE c.customer_id = :customerId ORDER BY s.start_date DESC LIMIT 1", nativeQuery = true)
-    CustomerSummary findMostRecentCustomerSummary(@Param("customerId") Long customerId);
+    Optional<CustomerSummary> findMostRecentCustomerSummary(@Param("customerId") Long customerId);
 }
