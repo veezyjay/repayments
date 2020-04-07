79fb5ffb11519f50e06ce17543a104704bc59af1
Victor Bassey <viktorbasi545@gmail.com>
2020-04-07 09:14:34 +0100

    Add repayment entity
    #1

---
+++ src/main/java/com/victorbassey/repayment/model/Repayment.java
@@ -0,0 +1,93 @@
+package com.victorbassey.repayment.model;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "repayments")
+public class Repayment {
+    @Id
+    @GeneratedValue
+    private Long repaymentId;
+    private Long customerId;
+    private Long seasonId;
+
+    @CreationTimestamp
+    private LocalDate date;
+    private Long amount;
+    private Long parentId;
+
+    public Repayment() {
+    }
+
+    public Repayment(Long customerId, Long seasonId, Long amount) {
+        this.customerId = customerId;
+        this.seasonId = seasonId;
+        this.amount = amount;
+    }
+
+    public Long getRepaymentId() {
+        return repaymentId;
+    }
+
+    public void setRepaymentId(Long repaymentId) {
+        this.repaymentId = repaymentId;
+    }
+
+    public Long getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Long customerId) {
+        this.customerId = customerId;
+    }
+
+    public Long getSeasonId() {
+        return seasonId;
+    }
+
+    public void setSeasonId(Long seasonId) {
+        this.seasonId = seasonId;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public Long getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Long amount) {
+        this.amount = amount;
+    }
+
+    public Long getParentId() {
+        return parentId;
+    }
+
+    public void setParentId(Long parentId) {
+        this.parentId = parentId;
+    }
+
+    @Override
+    public String toString() {
+        return "Repayment{" +
+                "repaymentId=" + repaymentId +
+                ", customerId=" + customerId +
+                ", seasonId=" + seasonId +
+                ", date=" + date +
+                ", amount=" + amount +
+                ", parentId=" + parentId +
+                '}';
+    }
+}
