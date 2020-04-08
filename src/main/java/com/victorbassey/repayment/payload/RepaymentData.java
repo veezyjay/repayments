06bb2f26e48af0e7090ad395f5b1527d96c9481e
Victor Bassey <viktorbasi545@gmail.com>
2020-04-08 08:11:09 +0100

    Add updated customer summaries to payload

--- src/main/java/com/victorbassey/repayment/payload/RepaymentData.java
+++ src/main/java/com/victorbassey/repayment/payload/RepaymentData.java
@@ -1,18 +1,22 @@
 package com.victorbassey.repayment.payload;
 
+import com.victorbassey.repayment.model.CustomerSummary;
 import com.victorbassey.repayment.model.Repayment;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class RepaymentData {
     private Repayment originalRepayment;
     private List<Repayment> adjustmentRepayments;
+    private List<CustomerSummary> updatedSummaries;
 
     public RepaymentData() {
     }
 
     public RepaymentData(Repayment originalRepayment) {
         this.originalRepayment = originalRepayment;
+        this.updatedSummaries = new ArrayList<>();
     }
 
     public Repayment getOriginalRepayment() {
@@ -23,19 +27,28 @@ public class RepaymentData {
         this.originalRepayment = originalRepayment;
     }
 
-    public List<Repayment> getAdjustmentRepayment() {
+    public List<Repayment> getAdjustmentRepayments() {
         return adjustmentRepayments;
     }
 
-    public void setAdjustmentRepayment(List<Repayment> adjustmentRepayment) {
-        this.adjustmentRepayments = adjustmentRepayment;
+    public void setAdjustmentRepayments(List<Repayment> adjustmentRepayments) {
+        this.adjustmentRepayments = adjustmentRepayments;
+    }
+
+    public List<CustomerSummary> getUpdatedSummaries() {
+        return updatedSummaries;
+    }
+
+    public void setUpdatedSummaries(List<CustomerSummary> updatedSummaries) {
+        this.updatedSummaries = updatedSummaries;
     }
 
     @Override
     public String toString() {
         return "RepaymentData{" +
                 "originalRepayment=" + originalRepayment +
-                ", adjustmentRepayment=" + adjustmentRepayments +
+                ", adjustmentRepayments=" + adjustmentRepayments +
+                ", updatedSummaries=" + updatedSummaries +
                 '}';
     }
 }
