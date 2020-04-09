6963e1eda8250b91c0054c8cdc981ab43772742f
Victor Bassey <viktorbasi545@gmail.com>
2020-04-09 07:02:46 +0100

    Add react components
    Closes #8

---
+++ src/main/resources/static/precache-manifest.05de348f2731ac83caa42ae4586ec51c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46e748ba3c4dd05340d8288c5e3b5c73",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cabdadb112646c51f7b0",
+    "url": "/static/css/2.1cbea75e.chunk.css"
+  },
+  {
+    "revision": "53397a8695ed331fd79c",
+    "url": "/static/css/main.9e4b90b0.chunk.css"
+  },
+  {
+    "revision": "cabdadb112646c51f7b0",
+    "url": "/static/js/2.52064d57.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.52064d57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53397a8695ed331fd79c",
+    "url": "/static/js/main.071564f5.chunk.js"
+  },
+  {
+    "revision": "c24484bd310b524e67c5",
+    "url": "/static/js/runtime-main.90d40847.js"
+  },
+  {
+    "revision": "ca0876b08f0635555ec07534b02e5cd1",
+    "url": "/static/media/farms1.ca0876b0.jpg"
+  },
+  {
+    "revision": "b1ce103d3bc90606f8dfd86f07a38e6b",
+    "url": "/static/media/logo.b1ce103d.svg"
+  }
+]);
---
+++ src/main/resources/static/static/precache-manifest.8321620dbba772a3cb73e0469dabb0dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6261dfb13e72aefd8dca197f0f9ca59",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4cfe1e9d9bc8c2a2de59",
+    "url": "/static/css/2.34749746.chunk.css"
+  },
+  {
+    "revision": "a5322806e2201ab5286e",
+    "url": "/static/css/main.b918d2db.chunk.css"
+  },
+  {
+    "revision": "4cfe1e9d9bc8c2a2de59",
+    "url": "/static/js/2.861d99a8.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/static/js/2.861d99a8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a5322806e2201ab5286e",
+    "url": "/static/js/main.ba28fe4a.chunk.js"
+  },
+  {
+    "revision": "67d8378025b1305571b6",
+    "url": "/static/js/runtime-main.0650196e.js"
+  }
+]);
